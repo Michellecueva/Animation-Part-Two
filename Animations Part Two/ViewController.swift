02f4a454f320c208eefdb1acc6ca486bf3d55b5e
Michelle <Michelle.cueva@gmail.com>
2019-10-15 13:41:40 -0400

    Initial Commit

---
+++ Animations Part Two/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Animations Part Two
+//
+//  Created by Michelle Cueva on 10/15/19.
+//  Copyright © 2019 Michelle Cueva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
